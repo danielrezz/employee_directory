fc55a6c3eb79cf6eed829b56f5d323d740cfbd7d
Daniel Reza <daniel.reza1007@gmail.com>
2021-03-10 22:45:36 -0600

    adding table component

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Table from "./components/Table"
 import './App.css';
 
 function App() {
   return (
     <>
-      <Header />;
+      <Header />
+      <Table />
     </>
   );
 }
