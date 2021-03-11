4c10e61ae6d92a0ec809ff01a20773bd6c66b79f
Daniel Reza <daniel.reza1007@gmail.com>
2021-03-10 22:32:56 -0600

    creating header component

---
+++ employee_directory/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+    return (
+        <>
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">The Best Employee Directory</h1>
+                    <p class="lead">All your peers. All right heres.</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
